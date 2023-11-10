be9d6ad2f3c545b46b37ea91f5031487b4f32abd
MatveyFilippov <114354000+MatveyFilippov@users.noreply.github.com>
2023-11-11 01:57:31 +0300

    add func about workers

--- bf_connection.py
+++ bf_connection.py
@@ -9,7 +9,7 @@ import hashlib
 from settings import today_date, path2jmc
 
 
-def get_user_dict(worker_id: str, date=today_date) -> dict:
+def get_worker_dict(worker_id: str, date=today_date) -> dict:
     """
     Получи словарь работника:
 
@@ -65,7 +65,7 @@ def __generate_login__(fio: str) -> str:
     if len(first_letter) != 3:
         raise Exception(f"Ошибка с ФИО сотрудника --- скорее всего что-то с пробелами ('{fio}')")
 
-    before_num = f'{first_letter[0][0]}{first_letter[1][0]}{first_letter[2][0]}_'
+    before_num = f'{first_letter[0][0]}{first_letter[1][0]}{first_letter[2][0]}_'.upper()
     num = 100
     login = before_num + str(num)
 
@@ -88,7 +88,7 @@ def registrate_new_worker(fio: str, address: str, grad: str) -> dict[str, str]:
     """
     if grad not in ['Синьор', 'Мидл', 'Джун']:
         raise Exception(f"Ошибка с грейдом нового сотрудника, '{grad}' нельзя использовать")
-    # TODO: проверить адрес на пригодность
+    # TODO: проверить адрес на пригодность иначе raise KeyError
 
     workers_list = pd.read_excel(path2dataset, sheet_name='Справочник сотрудников')
     col = list(workers_list.columns)
@@ -113,7 +113,7 @@ def registrate_new_worker(fio: str, address: str, grad: str) -> dict[str, str]:
     return {'Логин': worker_login, 'Пароль': worker_pass}
 
 
-def login_user(login: str, password: str) -> dict:
+def login_worker(login: str, password: str) -> dict:
     """
     Проверка логина/пароля пользователя
 
@@ -134,6 +134,8 @@ def login_user(login: str, password: str) -> dict:
     }
     Это на сегодняшнюю дату будет
 
+    Если работник есть, а задач нет, то выбросит ошибку KeyError
+
     :param login: Логин работника
     :param password: Пароль работника
     :return: Словарь если есть работник, Ошибку (KeyError) если нет такого
@@ -143,14 +145,13 @@ def login_user(login: str, password: str) -> dict:
     if len(list(input_data.values)) != 1:
         raise KeyError('Ошибка в логине')
     elif list(input_data.values)[0][1] == __get_sha256_hash__(password):
-        return get_user_dict(login)
+        return get_worker_dict(login)   # TODO: Если работник есть, а задач нет, то выбросит ошибку KeyError
     else:
         raise KeyError('Ошибка в пароле')
 
 
-# TODO: функция удаления/изменения работника
+# TODO: функция изменения работника
 # TODO: функция удаления/изменения точки
-# TODO: get_all_workers()
 
 
 def switch_task_status(worker_id: str, name_of_street: str):
@@ -162,7 +163,7 @@ def switch_task_status(worker_id: str, name_of_street: str):
     :param worker_id: id работника
     :param name_of_street: улица, на которй он выполнил задание
     """
-    worker = get_user_dict(worker_id)
+    worker = get_worker_dict(worker_id)
     for name_of_task in worker["Задания"][name_of_street]:
         if worker["Задания"][name_of_street][name_of_task] == 'Не выполнено':
             worker["Задания"][name_of_street][name_of_task] = 'Выполнено'
@@ -195,3 +196,83 @@ def get_all_workers() -> list[list[int | str]]:
         result.append(worker)
 
     return result
+
+
+def del_worker(worker_id: str):
+    """
+    Удаляет работника из таблицы по его ID
+
+    :param worker_id: ID работника
+    """
+    workers_list = pd.read_excel(path2dataset, sheet_name=None)
+    worker_line = workers_list['Справочник сотрудников'].loc[:, ["ID"]]
+    worker_line = worker_line[
+        (worker_line['ID'] == worker_id)
+    ]
+    try:
+        ind = worker_line.index.values[0]
+    except IndexError:
+        raise KeyError(f"Пользователя с id: '{worker_id}' нет в таблице")
+    workers_list['Справочник сотрудников'].drop(ind, inplace=True)
+    with pd.ExcelWriter(path2dataset) as writer:
+        for sheet, data in workers_list.items():
+            data.to_excel(writer, sheet_name=sheet, index=False, header=True)
+
+
+def change_worker_params(worker_id: str, address: str | None = None, fio: str | None = None, grad: str | None = None):
+    """
+    Изменяет параметры сотрудника, при этом параметры - необязательные, можно передать от 0 до 3 за раз
+
+    :param worker_id: id работника, которого нужно отредактировать
+    :param address: если есть, то должен начинаться с 'Краснодар, '
+    :param fio: если есть, то должны быть Фамилия Имя Отчество (с пробелами)
+    :param grad: если есть, то должно быть ('Синьор', 'Мидл', 'Джун')
+    """
+    if not address and not fio and not grad:
+        return
+
+    worker_dict = get_workers(path2dataset)[worker_id]
+    if address:
+        # TODO: проверить адрес на пригодность иначе raise KeyError
+        worker_dict['Адрес'] = address
+    if fio:
+        worker_dict['ФИО'] = fio
+    if grad:
+        if grad in ['Синьор', 'Мидл', 'Джун']:
+            worker_dict['Грейд'] = grad
+        else:
+            raise KeyError(f'Неверный грейд сотрудника - {grad}')
+
+    workers_list = pd.read_excel(path2dataset, sheet_name='Справочник сотрудников')
+    col = list(workers_list.columns)
+    worker_line = workers_list.loc[:, ["ID", "Пароль"]]
+    worker_line = worker_line[
+        (worker_line['ID'] == worker_id)
+    ]
+    try:
+        ind = worker_line.index.values[0]
+        password = worker_line.values[0][1]
+    except IndexError:
+        raise KeyError(f"Пользователя с id: '{worker_id}' нет в таблице")
+
+    workers_list.drop(ind, inplace=True)
+    new_line = pd.DataFrame(
+        [
+            [worker_id, worker_dict['Адрес'], worker_dict['Грейд'], worker_dict['ФИО'], password]
+        ],
+        columns=col
+    )
+
+    book = pd.read_excel(path2dataset, sheet_name=None)
+    book['Справочник сотрудников'] = pd.concat([workers_list, new_line], ignore_index=True)
+
+    with pd.ExcelWriter(path2dataset) as writer:
+        for sheet, data in book.items():
+            data.to_excel(writer, sheet_name=sheet, index=False, header=True)
+
+
+def get_motivation() -> str:
+    """
+    Возвращает мотивационную фразу
+    """
+    return 'Ты сможешь'  # TODO: я допилю чтоб возвращал норм, пока можешь тестить так
